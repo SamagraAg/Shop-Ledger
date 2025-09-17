3adc6d090e6ca22ffb88e70afe1088686228d443
Samagra Agrawal <samagra110@gmail.com>
2025-09-17 14:29:59 +0530

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import cors from "cors";
 import connectDB from "./config/database.js";
 import path from "path";
 import dotenv from "dotenv";
-import authRouter from "./routes/auth.js"
-import customersRouter from "./routes/customers.js"
-import transactionsRouter from "./routes/transactions.js"
+import authRouter from "./routes/auth.js";
+import customersRouter from "./routes/customers.js";
+import transactionsRouter from "./routes/transactions.js";
 
 import User from "./models/User.js";
 import Customer from "./models/Customer.js";
@@ -25,15 +25,15 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use(authRouter);
-app.use(customersRouter);
-app.use(transactionsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/customers", customersRouter);
+app.use("/api/transactions", transactionsRouter);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 // Basic route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
