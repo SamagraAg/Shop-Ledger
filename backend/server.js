f879ce8b6c640fb4ac7160715e6ce4aae1fa5aad
Samagra Agrawal <samagra110@gmail.com>
2025-09-17 11:18:23 +0530

    Update server.js to serve static files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/database");
+const path = require("path");
 require("dotenv").config();
 
 const User = require("./models/User.js");
@@ -10,6 +11,8 @@ const Transaction = require("./models/Transaction.js");
 // Connect to database
 connectDB();
 
+const __dirname = path.resolve();
+
 const app = express();
 
 // Middleware
@@ -17,13 +20,15 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./routes/auth.js'));
-app.use('/api/customers', require('./routes/customers.js'));
-app.use('/api/transactions', require('./routes/transactions.js'));
+app.use("/api/auth", require("./routes/auth.js"));
+app.use("/api/customers", require("./routes/customers.js"));
+app.use("/api/transactions", require("./routes/transactions.js"));
+
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // Basic route
-app.get("/", (req, res) => {
-  res.json({ message: "Shop Ledger API is running!" });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
